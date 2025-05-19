a2f5fc9ad0e2af095ae727830607ee06c23d01ee
Thomas Yemane <Tomblue246@gmail.com>
2025-05-18 17:32:58 -0700

    database file added to config

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,12 +1,9 @@
 module.exports = {
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8000,
+    dbFile: process.env.DB_FILE,
+    jwtConfig: {
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN
     }
-  }
-};
+  };
