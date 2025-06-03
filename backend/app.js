974f3e356e4037498c4c2869f4ce259c15a81291
Thomas Yemane <Tomblue246@gmail.com>
2025-06-02 18:40:51 -0700

    updating appjs

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 if (!isProduction) {
   app.use(cors({
-       origin: 'http://localhost:5173',
+       origin: ['http://localhost:5173', 'http://localhost:8000'],
        credentials: true
      }));
 }
