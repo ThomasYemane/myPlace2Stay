6014ab7d0af34f80eb05aa497c12771fec107050
Thomas Yemane <Tomblue246@gmail.com>
2025-06-16 21:09:00 -0700

    .env production

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.MODE === 'production'
-  ? 'https://place2stay-backend.onrender.com'
+  ? 'https://myplace2stay.onrender.com'
   : 'http://localhost:8000';
 
 export default API_BASE_URL;
